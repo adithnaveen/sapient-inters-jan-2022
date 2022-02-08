cb728cd686664e785741f92737378d4f06939f43
Naveen <adith.naveen@gmail.com>
2022-02-08 09:07:36 +0530

    updated after log4j

--- workspaces/java-works/src/main/java/com/naveen/HelloWolrd.java
+++ workspaces/java-works/src/main/java/com/naveen/HelloWolrd.java
@@ -1,5 +1,9 @@
 package com.naveen;
- 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 
 public class HelloWolrd {
 	public String sayHi() {
@@ -11,7 +15,13 @@ public class HelloWolrd {
 		 
 	}
 	
+	
+	static Logger logger = LoggerFactory.getLogger(HelloWolrd.class); 
+	
+	
 	public static void main(String[] args) {
 		new HelloWolrd().hello();
+		logger.info("hello info");
+		logger.debug("hello debug");
 	}
 }
