f27e93e46b6fa274e42834b37bd72c47451f95f9
Naveen <adith.naveen@gmail.com>
2022-02-10 16:07:25 +0530

    updated reference for tomcat

--- Delete.java
+++ Delete.java
@@ -1,13 +1,17 @@
-package com.naveen; 
+package com.naveen;
 
 class Delete {
     public static void main(String args[]) {
-        System.out.println("Hello");
-
+        System.out.println("HELLOOOOOO");
 
         for (int i = 0; i < args.length; i++) {
-            System.out.println(" Value of I is  " +i);
+            System.out.println(" Value of I is  " + i);
         }
 
+    }
+
+    // this is new code
+    public void hi() {
+        // something goes here
     }
 }
