867da38c1ac9ef3f2d21c688734ed733f99bc28d
Naveen <adith.naveen@gmail.com>
2022-02-11 15:19:01 +0530

    update after jsp

--- workspaces/web-works/src/main/java/com/naveen/controller/LoginValidateServlet.java
+++ workspaces/web-works/src/main/java/com/naveen/controller/LoginValidateServlet.java
@@ -24,13 +24,19 @@ public class LoginValidateServlet extends HttpServlet {
 		login.setPassword(password);
 		
 		if(new LoginDao().loginValidate(login)) {
-			// invoke login success servlet 
-			request.getRequestDispatcher("success").forward(request, response);
+//			// invoke login success servlet 
+//			request.getRequestDispatcher("success").forward(request, response);
+			
+			// call the jsp 
+			String uri = "WEB-INF/pages/dashboard.jsp"; 
+			request.setAttribute("company-name", "SAPIENT INDIA PRIVATE LTD");
+			request.setAttribute("login", new Login("DUMMY", "DUYMMY"));
+			request.getRequestDispatcher(uri).forward(request, response);
 		}else {
 			// show the login form with the error message 
 			System.out.println("in else case.. ");
 			response.setContentType("text/html"); 
-			response.getWriter().println("<p style='color:red'>Please Enter valid Credentails</p>");
+			response.getWriter().println("<p style='color:red;text-align:center'>Please Enter valid Credentails</p>");
 			request.getRequestDispatcher("/login-form.html").include(request, response); 
 			
 		}
