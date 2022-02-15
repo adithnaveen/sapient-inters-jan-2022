bf2fd4d1c4fcef116b281d34da6f4542b964443e
Naveen <adith.naveen@gmail.com>
2022-02-15 11:53:30 +0530

    updated with arch diagram

---
+++ spring-boot-projects/rest-works/src/main/java/com/naveen/rest/works/RestWorksApplication.java
@@ -0,0 +1,16 @@
+package com.naveen.rest.works;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class RestWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWorksApplication.class, args);
+	}
+
+	
+}
