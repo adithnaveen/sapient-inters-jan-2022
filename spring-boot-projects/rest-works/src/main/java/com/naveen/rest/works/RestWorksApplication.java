7e9e14dc9c74a72c83fab2ce31e9ce985692942b
Naveen <adith.naveen@gmail.com>
2022-02-16 15:49:25 +0530

    updated code with update,delete, hal and swagger

--- spring-boot-projects/rest-works/src/main/java/com/naveen/rest/works/RestWorksApplication.java
+++ spring-boot-projects/rest-works/src/main/java/com/naveen/rest/works/RestWorksApplication.java
@@ -5,6 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+// to invoke swagger 
+//http://localhost:9090/v3/api-docs/
+//http://localhost:9090/swagger-ui/index.html
+
+
+// to invoke hal browser 
+//http://localhost:9090/
+
+
 @SpringBootApplication
 public class RestWorksApplication {
 
--- spring-boot-projects/rest-works/src/main/java/com/naveen/rest/works/controller/UserController.java
+++ spring-boot-projects/rest-works/src/main/java/com/naveen/rest/works/controller/UserController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +23,7 @@ public class UserController {
 	@Autowired
 	private UserService service;
 
-	@GetMapping("/")
+	@GetMapping("/health")
 	public String sayHello() {
 		return "Service Is Up";
 	}
@@ -53,11 +55,37 @@ public class UserController {
 	
 	
 	
+	
 	// PUT -> update
+	@PutMapping("/user")
+	public ResponseEntity<?> updateUser(@RequestBody User user) throws UserNotFoundException{
+		
+		if(service.getUserById(user.getId()).isPresent()) {
+			User updateUser = service.updateUser(user);
+			return ResponseEntity.status(HttpStatus.OK).body(updateUser);
+		} else {
+			throw new UserNotFoundException("User Not Found to Update" + user.toString());
+		}
+	}
 	// DELETE -> delete 
+	@DeleteMapping("/user/{id}")
+	public ResponseEntity<?> deleteUser(@PathVariable Integer id) throws UserNotFoundException{
+		
+		if(service.getUserById(id).isPresent()) {
+			service.deleteUser(id); 
+			return ResponseEntity.status(HttpStatus.OK).body("User Deleted " + id);
+		} else {
+			throw new UserNotFoundException("User Not Found to Delete -> " +id);
+		}
+	}
+	
+	
+	
 	// GET - get users by name 
-	
-	
+	@GetMapping("/name/user/{name}")
+	public List<User> getUsersByName(@PathVariable String name) {
+		return service.getUsersByName(name); 
+	}
 }
 
 
